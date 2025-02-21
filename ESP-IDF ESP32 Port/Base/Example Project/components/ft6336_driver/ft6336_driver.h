cefa196c4d60ff99ecaa6bf16b2ff3bc4df4dc29
Mathrix <68941653+Dreams-Possible@users.noreply.github.com>
2025-02-21 20:43:45 +0800

    Add files via upload

---
+++ ESP-IDF ESP32 Port/Base/Example Project/components/ft6336_driver/ft6336_driver.h
@@ -0,0 +1,18 @@
+//enable ft6336 driver
+#if 1
+#ifndef FT6336_DRIVER_H
+#define FT6336_DRIVER_H
+
+//include
+#include<stdio.h>
+#include<stdint.h>
+
+//ft6336 init
+void ft6336_init();
+//ft6336 id
+uint8_t ft6336_id();
+//ft6336 read
+uint8_t ft6336_read(uint16_t*tc1,uint16_t*tc2);
+
+#endif//#ifndef FT6336_DRIVER_H
+#endif//#if 1
---
+++ ESP-IDF ESP32 Port/i2c_master/Example Project/main/hello_world_main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include"freertos/FreeRTOS.h"
+#include"freertos/task.h"
+
+#include"iic_driver.h"
+#include"ft6336_driver.h"
+
+void app_main(void)
+{
+    //iic init
+    iic_init();
+
+    //ft6336 init
+    ft6336_init();
+
+    uint16_t tc1[2]={0};
+    uint16_t tc2[2]={0};
+    while(1)
+    {
+        //ft6336 read
+        uint8_t num=ft6336_read(tc1,tc2);
+        if(num>0)
+        {
+            printf("x1=%d,y1=%d\n",tc1[0],tc1[1]);
+        }
+        if(num>1)
+        {
+            printf("x2=%d,y2=%d\n",tc2[0],tc2[1]);
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
